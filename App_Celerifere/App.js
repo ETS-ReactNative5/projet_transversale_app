f15822609769342beaa4b57fb0b6146df1dff321
melen-git <melen.lechat@gmail.com>
2020-03-19 16:56:34 +0100

    nouvelle branch avec nevigation et components d'affichage

--- App_Celerifere/App.js
+++ App_Celerifere/App.js
@@ -1,11 +1,16 @@
 import React from 'react'
+import 'react-native-gesture-handler'
+import {NavigationContainer} from '@react-navigation/native'
+import {createStackNavigator} from '@react-navigation/stack'
 import Map from './Components/Map'
+import Config from './Components/Config'
+import InfoAvanc from './Components/InfoAvanc'
+import Localisation from './Components/Localisation'
 import { StyleSheet, View, Text, Button} from 'react-native'
 import CompVitesse from './Components/CompVitesse'
 import CompBatterie from './Components/CompBatterie'
 
 export default class App extends React.Component {
-
   render() {
     return (
       <View style={styles.main_container}>
@@ -14,9 +19,9 @@ export default class App extends React.Component {
         </View>
         <View style={styles.bas_ecran}>
           <View style={styles.batterie_boutons}>
-            <Button title='<-' onPress={() => {}}/>
+            <Button title='Config' onPress={() => {navigation.navigate('Config')}}/>
             <CompVitesse/>
-            <Button title='->' onPress={() => {}}/>
+            <Button title='Infav' onPress={() => {navigation.navigate('InfoAvanc')}}/>
           </View>
           <CompBatterie style={styles.CompBatterie}/>
           <Text style={styles.texts}>{'A Propos'}</Text>
@@ -56,7 +61,6 @@ const styles = StyleSheet.create({
     textAlignVertical: 'center',
     marginHorizontal: 50,
   },
-
   texts: {
     flex: 1,
     paddingHorizontal: 25,
@@ -74,11 +78,20 @@ const styles = StyleSheet.create({
     borderRadius: 5,
     textAlign: 'center',
     textAlignVertical: 'center'
-  },
-  boutonsGD: {
-    height: 30,
-    width: 30,
-    borderRadius: 30,
-
   }
 })
+
+const Stack = createStackNavigator();
+
+function MyStack() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen  name="Accueil"  component={App}}  />
+        <Stack.Screen  name="Réglages"  component={Config}}  />
+        <Stack.Screen  name="Localisation"  component={Localisation}}  />
+        <Stack.Screen  name="Infos Avancées"  component={InfoAvanc}}  />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
